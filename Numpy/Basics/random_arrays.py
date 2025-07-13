0cc44dbdc2ce45dd6ae608a5289f52af241e1ae9
Vansh Sharma <xplore.vansh@gmail.com>
2025-07-13 10:45:42 +0530

    Random Arrays

---
+++ Numpy/Basics/random_arrays.py
@@ -0,0 +1,8 @@
+import numpy as np 
+zeros = np.zeros((2,3))
+ones = np.ones((3,2))
+randoms = np.random.rand(2,2)
+
+print("Zeros:\n ",zeros)
+print("Ones:\n ",ones)
+print("Random Number:\n ",randoms)
